68da8af6bb9a412dfe1ed48ac0917f0396aff4c0
3gzon <egzonuka8@gmail.com>
2026-04-01 19:08:35 +0200

    created jsx component and learn about app react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>
+                    <a href="">home</a>
+                </li>
+            </ul>
+       </header>
+    )
+}
+export default Header;
